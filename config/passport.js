eeb8f8f62a7d175427571090128aa4e100afb67f
destiny <destinyifeh@gmail.com>
2022-12-30 09:24:40 -1200

    updated

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,38 @@
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
 
+const User = require("../models/User");
 
-require('../models/User');
-
-module.exports= function(passport){
-    passport.use(new LocalStrategy({usernameField: 'username'}, (username, password, done)=>{
-        User.findOne({username: username})
-        .then(user=>{
-            if(!user){
-                return done(null, false, {message: 'No user with this account found'});
-            }
-            bcrypt.compare(password, user.password, (err, isMatch)=>{
-                if(err) throw err;
-                if(isMatch){
-                    return done(null, user);
-                }else{
-                    return done(null, false, {message: 'Password is incorrect'});
-                }
+module.exports = function (passport) {
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "username" },
+      (username, password, done) => {
+        User.findOne({ username: username }).then((user) => {
+          if (!user) {
+            return done(null, false, {
+              message: "No user with this account found",
             });
+          }
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err;
+            if (isMatch) {
+              return done(null, user);
+            } else {
+              return done(null, false, { message: "Password is incorrect" });
+            }
+          });
         });
-    }));
+      }
+    )
+  );
 
-    passport.serializeUser(function(user, done){
-        done(null, user.id);
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
     });
-    passport.deserializeUser(function (id, done){
-        User.findById(id, function (err, user){
-            done(err, user);
-        });
-    });
+  });
 };
--- routes/site.js
+++ routes/site.js
@@ -1,141 +1,146 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const nodemailer = require('nodemailer');
-const env = require('dotenv');
+const nodemailer = require("nodemailer");
+const env = require("dotenv");
 
-env.config({path: './.env'});
+env.config({ path: "./.env" });
 
-require('../models/Post');
-require('../models/User');
+const Post = require("../models/Post");
+const User = require("../models/User");
 
-const {checkUser} = require('../helpers/auth');
+const { checkUser } = require("../helpers/auth");
 
 //main page
 
-router.get('/', async(req, res)=>{
-  
-    //pagination
-    const page = parseInt(req.query.page)||1
-    const limit = parseInt(req.query.limit)||9
-    const count =await Post.countDocuments({});
-    const pages = Math.ceil(count/limit)
-    let nextIndex = (page+1)
-    let startIndex = (page-1)
-   
-     if(nextIndex > pages){
-        nextIndex=false;
-    }
-    let posts = await Post.find({}).sort({date:-1}).skip((limit*page)-limit).limit(limit);
+router.get("/", async (req, res) => {
+  //pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 9;
+  const count = await Post.countDocuments({});
+  const pages = Math.ceil(count / limit);
+  let nextIndex = page + 1;
+  let startIndex = page - 1;
 
-    res.render('index', {posts, page, limit, nextIndex, startIndex, title: 'Basicoffer'});
+  if (nextIndex > pages) {
+    nextIndex = false;
+  }
+  let posts = await Post.find({})
+    .sort({ date: -1 })
+    .skip(limit * page - limit)
+    .limit(limit);
+
+  res.render("index", {
+    posts,
+    page,
+    limit,
+    nextIndex,
+    startIndex,
+    title: "Basicoffer",
+  });
 });
 
 //about page
 
-
-router.get('/about', (req, res)=>{
-    res.render('about');
+router.get("/about", (req, res) => {
+  res.render("about");
 });
 
 //contact page
 
-
-router.get('/contact', (req, res)=>{
-    res.render('contact');
+router.get("/contact", (req, res) => {
+  res.render("contact");
 });
-
 
 //disclaimer page
 
-
-router.get('/disclaimer', (req, res)=>{
-    res.render('disclaimer');
+router.get("/disclaimer", (req, res) => {
+  res.render("disclaimer");
 });
 
 //privacy page
 
-
-router.get('/privacy', (req, res)=>{
-    res.render('privacy');
+router.get("/privacy", (req, res) => {
+  res.render("privacy");
 });
 
 //Getting admin page//
-router.get('/admin', checkUser, async(req, res)=>{
-   
-    //pagination
-    const page = parseInt(req.query.page)||1
-    const limit = parseInt(req.query.limit)||7
-    const count =await Post.countDocuments({});
-    const pages = Math.ceil(count/limit)
-    let nextIndex = (page+1)
-    let startIndex = (page-1)
-   
-     if(nextIndex > pages){
-        nextIndex=false;
-    }
-    let posts = await Post.find({}).sort({date:-1}).skip((limit*page)-limit).limit(limit);
-    let tops = await Post.find({}).sort({views:-1}).limit(3);
-    res.render('dashboard', {posts,tops, page, limit, nextIndex, startIndex, title: 'Admin - Basicoffer'});
-});
+router.get("/admin", checkUser, async (req, res) => {
+  //pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 7;
+  const count = await Post.countDocuments({});
+  const pages = Math.ceil(count / limit);
+  let nextIndex = page + 1;
+  let startIndex = page - 1;
 
+  if (nextIndex > pages) {
+    nextIndex = false;
+  }
+  let posts = await Post.find({})
+    .sort({ date: -1 })
+    .skip(limit * page - limit)
+    .limit(limit);
+  let tops = await Post.find({}).sort({ views: -1 }).limit(3);
+  res.render("dashboard", {
+    posts,
+    tops,
+    page,
+    limit,
+    nextIndex,
+    startIndex,
+    title: "Admin - Basicoffer",
+  });
+});
 
 //Getting search for admin//
 
-router.get('/find', async(req, res)=>{
-    const {query} = req.query;
-    let q = new RegExp(query, 'i');
-    let finds = await Post.find({$or:[{title:q}, {detail:q}]});
-    res.render('dashboard', {finds, query});
+router.get("/find", async (req, res) => {
+  const { query } = req.query;
+  let q = new RegExp(query, "i");
+  let finds = await Post.find({ $or: [{ title: q }, { detail: q }] });
+  res.render("dashboard", { finds, query });
 });
-
 
 //contact form
 
-
-router.post('/contact', async(req, res)=>{
-    try{
-   
-    
-     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        tls:{
-            rejectUnauthorized: false,
-        },
-        auth:{
-         user: process.env.GMAIL_EMAIL,
-         pass:process.env.GMAIL_PASS
-        },
+router.post("/contact", async (req, res) => {
+  try {
+    let transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      tls: {
+        rejectUnauthorized: false,
+      },
+      auth: {
+        user: process.env.GMAIL_EMAIL,
+        pass: process.env.GMAIL_PASS,
+      },
     });
 
-     var mailOptions={
-                   from: req.body.sender,
-                   to: process.env.GMAIL_EMAIL,
-                   replyTo: req.body.sender,
-                   subject: 'Message from' +' '+ req.body.sender,
-                   text: req.body.contactBody,
-          };
-       
-         transporter.sendMail(mailOptions, function(err, info){
-           if(err){
-               console.log(err)
-               req.flash('error_msg', 'Message not sent, try again')
-               return res.redirect('back')
-           }else{
-               console.log('Message sent successfully' + info.response)
-               req.flash('success_msg', 'Message delivered successfully')
-               return res.redirect('/');
-           }
-       })
-    }
-    catch(err){
-        console.log(err.message)
-        res.redirect('/500');
-    }
+    var mailOptions = {
+      from: req.body.sender,
+      to: process.env.GMAIL_EMAIL,
+      replyTo: req.body.sender,
+      subject: "Message from" + " " + req.body.sender,
+      text: req.body.contactBody,
+    };
+
+    transporter.sendMail(mailOptions, function (err, info) {
+      if (err) {
+        console.log(err);
+        req.flash("error_msg", "Message not sent, try again");
+        return res.redirect("back");
+      } else {
+        console.log("Message sent successfully" + info.response);
+        req.flash("success_msg", "Message delivered successfully");
+        return res.redirect("/");
+      }
+    });
+  } catch (err) {
+    console.log(err.message);
+    res.redirect("/500");
+  }
 });
-
-
-
 
 module.exports = router;
